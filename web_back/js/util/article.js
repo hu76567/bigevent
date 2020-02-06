9ba4ed7881e47cf1937a1a020c1fda3c5fede297
iq7ii <385897888@qq.com>
2020-02-06 20:50:09 +0800

    文章列表筛选

--- web_back/js/util/article.js
+++ web_back/js/util/article.js
@@ -1,8 +1,8 @@
 // 把与文章分类相关的操作写在这里
 var article = {
     // 获取文章列表
-    get: function (page) {
-        return $.get(APILIST.article_get, { 'page': page })
+    get: function (page, type, status) {
+        return $.get(APILIST.article_get, { 'page': page, 'type': type, 'state': status })
     },
     // // 添加文章类别
     // add: function (name, slug) {
