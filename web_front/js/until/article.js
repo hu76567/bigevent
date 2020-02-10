5e08245b8703fbbc992bab22bffb131028dca926
iq7ii <385897888@qq.com>
2020-02-10 15:57:30 +0800

    列表页分页

--- web_front/js/until/article.js
+++ web_front/js/until/article.js
@@ -5,12 +5,33 @@ var article = {
     getFocus: function (page, type, status) {
         return $.get(APILIST.article_get, { perpage: 5 })
     },
+
+
+    /**
+    *  根据文章id去获取
+    * @param {Number} id 
+    */
+    getById: function (id) {
+        return $.get(APILIST.article_get, { 'id': id })
+    },
+
+
     // 获取排行
     getRank: function (type) {
         return $.get(APILIST.article_rank, { 'type': type })
     },
+
+
     // 获取最新资讯
     getLastest: function () {
         return $.get(APILIST.article_last)
     },
+
+
+    // 获取当前类别的文章列表
+    getList: function (type, page) {
+        return $.get(APILIST.article_get, { "type": type, "page": page })
+    },
+
+
 }
